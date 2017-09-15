0a9c971a9cdf40718f275acee4fae6d4d079aa5e
LedVaca <luisrazo@live.com.mx>
2017-09-14 21:56:31 -0500

    Script_enemy
    se agrega el respawner de los enemigos

---
+++ Spawnerenemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerenemy : MonoBehaviour {
+
+
+	[SerializeField] GameObject Enemy;
+	// Use this for initialization
+	void Start () {
+		
+			for (int i = -4; i < 5; i++)
+				{
+				for (int e = 0; e < 4 ;e++) {
+					Instantiate (Enemy, new Vector3 (i, e, 0), Quaternion.identity);
+			}
+	}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
